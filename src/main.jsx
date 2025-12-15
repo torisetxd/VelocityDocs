ed8688e1fa1f1fc9812bf68e3fade10a5ea0e166
toriset <xdragongamer14@gmail.com>
2025-12-16 01:46:22 +0200

    set document title and favicon from config values

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { config } from './config.js'
+
+document.title = config.title;
+const link = document.querySelector("link[rel~='icon']");
+if (link) link.href = config.logo;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
